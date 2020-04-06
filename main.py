bfee6e5c43f610d85af81cd987145516fc8806f9
Polina Vinogradova <p.p.vinogradova@yandex.ru>
2020-04-06 15:32:01 +0000

    basical

--- main.py
+++ main.py
@@ -43,5 +43,54 @@ class Contact:
 
 jhon = Contact('Jhon', 'Smith', '+71234567809', False, 'любит хот-доги', 'выгуливает собак', telegram='@jhony', email='jhony@smith.com')
 
-if __name__ == '__main__':
-  print(jhon)
+mary = Contact('Mary', 'Johnsson', '+9097483553', True, 'массаж')
+
+kate = Contact('Kate', 'Wang', '+0765246789', True, telegram='@w_kate')
+
+data = [[jhon.name, jhon.surname, jhon.phone_num, jhon.fav, jhon.args, jhon.kwargs],[mary.name, mary.surname, mary.phone_num, mary.fav, mary.args, mary.kwargs], [kate.name, kate.surname, kate.phone_num, kate.fav, kate.args, kate.kwargs]]
+
+#print(type(jhon))
+
+class PhoneBook:
+
+  def __init__(self, book_name):
+    self.book_name = book_name
+    
+
+  def print_contacts(self):
+    if self.book_name == Contact:
+      for name in data:
+        print(name)
+
+  def add_contact(self, name, surname, phone_num, fav=False, *args, **kwargs):
+    if self.book_name == Contact:
+      info = [name, surname, phone_num, fav, args, kwargs]
+      data.append(info)
+      print(data)
+
+  def del_contact(self, number):
+    if self.book_name == Contact:
+      for contact in data:
+        if number in contact:
+          data.remove(contact)
+      print(data)
+
+  def find_fav(self):
+    if self.book_name == Contact:
+      for contact in data:
+        if True in contact:
+          print(contact)
+
+  def search(self, name, surname):
+    if self.book_name == Contact:
+      for contact in data:
+        if name and surname in contact:
+          print(contact)
+ 
+
+book = PhoneBook(Contact)
+# book.print_contacts()
+# book.add_contact('July', 'Fox', '+793745566')
+# book.del_contact('+71234567809')
+# book.find_fav()
+# book.search('Mary', 'Johnsson')
